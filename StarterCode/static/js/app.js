ae1042ea4d90d6b3ead713c8f40fa71b73a41e14
ShaeStringerJones <ShaeStringer@gmail.com>
2021-04-08 14:38:08 -0500

    data

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,5 @@
 // Use the D3 library to read in samples.json.
-d3.json("samples.json").then(function createPlotly(data) {
+d3.json("data/samples.json").then(function createPlotly(data) {
     console.log(data);
     var testid = data.names;
     console.log(testid);
